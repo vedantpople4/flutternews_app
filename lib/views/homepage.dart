b9bc3055797bc9cbe467f6141972fd206cc1b2f8
vedantpople <vedantpople4@gmail.com>
2020-07-27 16:53:25 +0530

    Update pubspec.yaml

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,1 +1,9 @@
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  bool _loading;
+  var newslist;
+}
