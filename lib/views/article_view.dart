44d33e9c91a34c9ee048550752e9ec3de5d1ead6
vedantpople <vedantpople4@gmail.com>
2020-07-31 16:12:09 +0530

    article_view update

---
+++ lib/views/article_view.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleView extends StatefulWidget {
+  final String postUrl;
+  ArticleView({@required this.postUrl});
+
+  @override
+  _ArticleViewState createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {}
