fdf1b0423bd2efb7b91f3a9a1259a54a8e1c7a1d
vedantpople <vedantpople4@gmail.com>
2020-07-31 16:03:20 +0530

    update categorie_model.dart

--- lib/models/categorie_model.dart
+++ lib/models/categorie_model.dart
@@ -1,13 +1,4 @@
-import 'package:flutter/material.dart';
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool loading;
-  var newslist;
-  List<CategorieModel> categories = List<CategorieModel>();
-  void getNews() async {}
+class CategorieModel {
+  String imageAssetUrl;
+  String categorieName;
 }
