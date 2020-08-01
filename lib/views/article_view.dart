33c13ea4a01d04560d1b38cefaf8c56a1ed8fff6
vedantpople <vedantpople4@gmail.com>
2020-08-01 18:00:37 +0530

    Update article_view.dart

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material';
+import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
@@ -12,4 +13,39 @@ class ArticleView extends StatefulWidget {
   _ArticleViewState createState() => _ArticleViewState();
 }
 
-class _ArticleViewState extends State<ArticleView> {}
+class _ArticleViewState extends State<ArticleView> {
+  final Complete<WebViewController> _controller =
+      Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Flutter",
+              style:
+                  TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
+            ),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.share))
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+      ),
+    );
+  }
+}
