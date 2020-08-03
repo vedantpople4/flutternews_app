dd160af47d89f5a4b8a93d3d45dbd55922895d6c
vedantpople <vedantpople4@gmail.com>
2020-08-03 14:15:10 +0530

    New categorie_news.dart

--- lib/views/categorie_news.dart
+++ lib/views/categorie_news.dart
@@ -32,30 +32,55 @@ class _CategoryNewsState extends State<CategoryNews> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Flutter",
-              style:
-                  TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
-            ),
-            Text(
-              "News",
-              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Flutter",
+                style: TextStyle(
+                    color: Colors.black87, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "News",
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+          actions: <Widget>[
+            Opacity(
+              opacity: 0,
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Icon(
+                    Icons.share,
+                  )),
             )
           ],
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
         ),
-        actions: <Widget>[
-          Opacity(
-            opacity: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-            ),
-          )
-        ],
-      ),
-    );
+        body: _loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Container(
+                    margin: EdgeInsets.only(top: 16),
+                    child: ListView.builder(
+                      itemCount: newslist.length,
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return NewsTile(
+                          imageUrl: newslist[index].urlToImage ?? "",
+                          title: newslist[index].title ?? "",
+                          desc: newslist[index].description ?? "",
+                          content: newslist[index].content ?? "",
+                          posturl: newslist[index].articleUrl ?? "",
+                        );
+                      },
+                    ))));
   }
 }
